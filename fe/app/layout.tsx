faaf2c3a7b691ace7aed4dc6a2cb994afb2d2678
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-04-16 16:46:34 +0700

    update 2 files, delete 33 files, move 4 files and create 1 file

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -1,5 +1,7 @@
-import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import Head from 'next/head';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -7,6 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+      <Head>
+        <title>My Clothing Store</title>
+        <meta name="description" content="Come to my store for great apparel!" />
+        <meta property="og:title" content="My Clothing Store" />
+        <meta property="og:description" content="Come to my store for great apparel!" />
+        <meta property="og:url" content="https://myclothingstore.com/" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
