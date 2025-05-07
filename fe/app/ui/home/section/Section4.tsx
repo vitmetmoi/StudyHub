681d847e625b4e1d61ae933c91b60a2589da0225
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-05-07 19:24:22 +0700

    update 2 files and create 3 files

--- fe/app/ui/home/section/Section4.tsx
+++ fe/app/ui/home/section/Section4.tsx
@@ -31,24 +31,34 @@ export default function Section4() {
                 <div className='card-group'>
 
                     <div className='card'>
-                        <div className='card-image'></div>
-                        <div className='title'></div>
-                        <div className='detail'></div>
-                        <button></button>
+                        <div className='card-image'>
+                            <img className='img' src="/section/Small1.png"></img>
+                        </div>
+                        <div className='title'>
+                            The "good" kind of <br></br>
+                            peer pressure</div>
+                        <div className='detail'>You know how your parents always say peer pressure is bad? Well… when it comes to studying, they’re wrong. Studying with peers helps you get better grades - and that’s scientifically proven.</div>
+                        <button>Read more <img src='/button.svg'></img></button>
                     </div>
 
                     <div className='card'>
-                        <div className='card-image'></div>
-                        <div className='title'></div>
-                        <div className='detail'></div>
-                        <button></button>
+                        <div className='card-image'>
+                            <img className='img' src="/section/Small2.png"></img>
+                        </div>
+                        <div className='title'>24/7 support,<br></br>
+                            all year round  </div>
+                        <div className='detail'>Find a study buddy (or a procrastinate-mate), access exclusive boot camps, chat with tutors, or ask for community help. When you need a break, take a 5 min guided mindfulness session.</div>
+                        <button>Read more <img src='/button.svg'></img></button>
                     </div>
 
                     <div className='card'>
-                        <div className='card-image'></div>
-                        <div className='title'></div>
-                        <div className='detail'></div>
-                        <button></button>
+                        <div className='card-image'>
+                            <img className='img' src="/section/Small3.png"></img>
+                        </div>
+                        <div className='title'>Studying,<br></br>
+                            reloaded </div>
+                        <div className='detail'>Let’s be honest, any task is more fun when you know there’s a reward at the end of it. We’ll track your progress and gamify your study sessions – all you have to do is set session goals, start the timer and you’ll get rewarded.</div>
+                        <button>Read more <img src='/button.svg'></img></button>
                     </div>
 
 
