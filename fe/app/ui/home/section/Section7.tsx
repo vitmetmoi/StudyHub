0a90b4e596883f157d60db8c38cfcbddabafcbd8
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-05-10 22:06:52 +0700

    update Section7.tsx

--- fe/app/ui/home/section/Section7.tsx
+++ fe/app/ui/home/section/Section7.tsx
@@ -3,12 +3,10 @@ import { useEffect, useState } from "react"
 import './Section7.scss';
 import RollingBanner from "@/app/lib/RollingBanner";
 
-type propsType = {
-    arrPathImage: string[]
-}
 
-export default function Section7(props: propsType) {
-    const { arrPathImage } = props;
+
+export default function Section7() {
+
 
     return (
         <>
