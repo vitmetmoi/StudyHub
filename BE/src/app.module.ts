6a4baa3668575c1b0c869bd9ccc828b357528e0e
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-05-14 22:24:26 +0700

    update 1 file and create 2 files

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserServiceService } from './user-service/user-service.service';
+import { ServicesService } from './services/services.service';
+import { ServiceService } from './service/service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserServiceService, ServicesService, ServiceService],
 })
 export class AppModule {}
