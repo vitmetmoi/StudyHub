9ddc040a64727cf5b35d0751fa8e3a968b6dd155
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-05-15 15:25:52 +0700

    delete 2 files, update 2 files and create 8 files

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService,],
+  imports: [AuthModule, CommonModule,],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoginService } from './login/login.service';
 import { LogoutService } from './logout/logout.service';
 import { RegisterService } from './register/register.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [LoginService, LogoutService, RegisterService]
+  providers: [LoginService, LogoutService, RegisterService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
